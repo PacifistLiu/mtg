b8a56f617f7e6050f5d3a17249bb52463698a1c3
PacifistLiu <knechtruprecht1986@gmail.com>
2024-10-13 18:43:22 +0200

    Add files via upload

--- assets/js/cardsearch.js
+++ assets/js/cardsearch.js
@@ -46,10 +46,11 @@ function searchCards(data) {
     const selectedRarities = Array.from(rarityCheckboxes).map(cb => cb.value);
     const selectedColors = Array.from(colorCheckboxes).map(cb => cb.value);
     const selectedColorScheme = colorMatchingScheme.value;
-    const cardColors = cardData.color.toLowerCase().split(",").map(color => color.trim());
+    
     
     // Filter the card data based on the search query and filters
     const searchResults = cardData.filter(card =>
+		const cardColors = card.color.toLowerCase().split(",").map(color => color.trim());
         (selectedType === 'all' || card.type.toLowerCase().includes(selectedType.toLowerCase())) &&
         (selectedCreatureType === 'all' || card.type.toLowerCase().includes(selectedCreatureType.toLowerCase())) &&
         (selectedSet === 'all' || card.set.toLowerCase().includes(selectedSet.toLowerCase())) &&
